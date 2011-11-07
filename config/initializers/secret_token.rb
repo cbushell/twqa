f7e3d896d95bb49308ef51ab7d2732b7146cb545
Chris Bushell <c.bushell@gmail.com>
2011-11-07 19:53:00 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twqa::Application.config.secret_token = 'fa1a5e22835f58003a5280a2003072a9b76e1fb2697ca2dfd7a2ae51a317986b4ce7705f04ce8481bc5f044a9ad075cfe6c2bf47aa5687a8bf3715ac0a51fd32'
