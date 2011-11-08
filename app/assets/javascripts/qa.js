33cc4fd854c010f73948d94ef624fb7784d75277
Chris Bushell <c.bushell@gmail.com>
2011-11-08 16:04:20 +1100

    adding hidden answer to dom

--- app/assets/javascripts/qa.js
+++ app/assets/javascripts/qa.js
@@ -7,11 +7,12 @@ $(document).ready(function() {
             var template =
                     "<div class='question'>" +
                             "<h2>{{question}}</h2>" +
-                            "<ol class='answers'>" +
+                            "<ol class='choices'>" +
                             "{{#choices}}" +
                             "<li><a href='#' class='btn large primary'>{{.}}</a></li>" +
                             "{{/choices}}" +
                             "</ol>" +
+                            "<div class='answer'>{{correct_choice}}</div>" +
                             "</div>";
             var question = Mustache.to_html(template, item)
             $("#questions").append(question);
--- app/controllers/qa_controller.rb
+++ app/controllers/qa_controller.rb
@@ -1,10 +1,5 @@
 class QaController < ApplicationController
 
-  def index
-    #number_of_questions = 3
-    #@questions = Questions.new.random(number_of_questions)
-  end
-
   def questions
     number_of_questions = 3
     @questions = Questions.new.random(number_of_questions)
