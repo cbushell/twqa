d89d47d353298b945670550437f3b40aaf751932
Chris Bushell <c.bushell@gmail.com>
2011-11-08 16:33:40 +1100

    identify correct answer and selected answer to questions

--- app/assets/javascripts/qa.js
+++ app/assets/javascripts/qa.js
@@ -9,15 +9,23 @@ $(document).ready(function() {
                             "<h2>{{question}}</h2>" +
                             "<ol class='choices'>" +
                             "{{#choices}}" +
-                            "<li><a href='#' class='btn large primary'>{{.}}</a></li>" +
+                            "<li>" +
+                            "<a href='#' class='btn large primary'>{{.}}</a>" +
+                            "<div class='answer'>{{correct_choice}}</div>" +
+                            "</li>" +
                             "{{/choices}}" +
                             "</ol>" +
-                            "<div class='answer'>{{correct_choice}}</div>" +
                             "</div>";
             var question = Mustache.to_html(template, item)
             $("#questions").append(question);
         });
 
     })
+
+    $("#questions").on("click", ".choices a", function(event) {
+        var choice = event.currentTarget.text;
+        var correctAnswer = $(event.target.parentElement).find(".answer").text()
+        
+    });
 
 });
